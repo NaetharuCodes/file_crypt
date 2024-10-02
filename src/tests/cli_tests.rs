a6d3cfeef3f2f8d311dd19e0d9d77520f367c2a5
naetharu <naetharu@gmail.com>
2024-10-03 00:19:14 +0100

    initial commit

---
+++ src/tests/cli_tests.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cli() {
+        // Your test code here
+    }
+}
---
+++ src/tests/file_ops_tests.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_file_ops() {
+        // Your test code here
+    }
+}
